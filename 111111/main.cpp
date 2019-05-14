64e89c85774076efe66539b611305a1e6dbb6ea2
maxim218 <31542700+maxim218@users.noreply.github.com>
2019-05-14 19:12:50 +0300

    22222
    222222

--- 111111/main.cpp
+++ 111111/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctime>
+#include <pthread.h>
 using namespace std;
 
 void longFunction() {
@@ -12,21 +13,46 @@ void longFunction() {
     }
 }
 
-int main() {
-    int a = 1;
-    int b = 2000;
+int a1, b1, s1;
+int a2, b2, s2;
 
-    int summa = 0;
-
-    for(int i = a; i <= b; i++) {
-        summa += i;
+void * first(void *) {
+    s1 = 0;
+    for(int i = a1; i <= b1; i++) {
+        s1 += i;
         longFunction();
     }
+}
 
-    cout << "\n";
-    cout << "Summa: " << summa << "\n";
-    cout << "\n";
-    
+void * second(void *) {
+    s2 = 0;
+    for(int i = a2; i <= b2; i++) {
+        s2 += i;
+        longFunction();
+    }
+}
+
+int main() {
+    a1 = 1;
+    b1 = 1000;
+
+    a2 = 1001;
+    b2 = 2000;
+
+    pthread_t x1;
+    pthread_t x2;
+
+    pthread_create(&x1, NULL, first, NULL);
+    pthread_create(&x2, NULL, second, NULL);
+
+    pthread_join(x1, NULL);
+    pthread_join(x2, NULL);
+
+    cout << "S1: " << s1 << "\n";
+    cout << "S2: " << s2 << "\n";
+    int summa = s1 + s2;
+    cout << "Summa: " << summa << "\n\n";
+
     cout << "Time: " << clock() << "\n\n";
 
     return 0;
